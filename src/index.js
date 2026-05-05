4d9a79633af9109153f195c9b9a6d5ae790c4ff3
OlgaFirs <odanilona21@gmail.com>
2026-05-05 15:49:13 +0400

    feat: implement human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  if (number < 20) {
+    return ones[number];
+  }
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  if (number < 100) {
+    const ten = Math.floor(number / 10);
+    const one = number % 10;
+
+    if (one === 0) {
+      return tens[ten];
+    }
+    return `${tens[ten]} ${ones[one]}`;
+  }
+
+  if (number < 1000) {
+    const hundred = Math.floor(number / 100);
+    const tens2 = number % 100;
+
+    if (tens2 === 0) {
+      return `${ones[hundred]} hundred`;
+    }
+
+    return `${ones[hundred]} hundred ${toReadable(tens2)}`;
+  }
+  return '';
 };
